e1336e4eee4c26154ef733efcb5aa715ef431ae6
fuzhao <fuzhao@datastory.com,.cn>
2019-04-11 19:42:45 +0800

    初始化项目

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+/**
+ * @Author: lifuzhao
+ * @Date: 2019-03-22
+ * @Project: vue-extension
+ */
+import EmptyComponent from './pages/empty'
+import App from './App'
+import Login from './pages/login'
+import Admin from './pages/admin'
+import MeituanCrawler from './pages/meituan/crawler'
+import MeituanList from './pages/meituan/list'
+import MeituanConfig from './pages/meituan/config'
+export default []
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: lifuzhao
+ * @Date: 2019-03-23
+ * @Project: vue-extension
+ */
+module.exports = {
+  devServer: {
+    proxy: 'https://www.meituan.com'
+  }
+}
